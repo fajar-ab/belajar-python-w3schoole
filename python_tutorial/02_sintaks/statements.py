66836b0935c2cb80f410716a90f1e2b6a4acdea2
fajar-ab <fajarfanz378@gmail.com>
2025-10-15 03:50:30 +0700

    update kemajuan belajar

---
+++ python_tutorial/02_sintaks/statements.py
@@ -0,0 +1,7 @@
+print("hello world!")
+print("have a good day.")
+print("learning python is fun!")
+
+# titik koma
+
+print("hello"); print("how are you?"); print("bye bye!")
---
+++ python_tutorial/03_keluaran/cetak_teks.py
@@ -0,0 +1,15 @@
+# cetak teks
+
+print("hello world!")
+print("i am learning python")
+print("it is awsome!")
+
+# double cuotes
+
+print("this still work")
+print('this still also work')
+
+# cetak tanpa garis baru
+
+print("hello world!", end=" ")
+print("i will print on the same line.")
